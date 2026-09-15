15cb72a701f8836af6361fc4cab3b43f154c6d8a
QTC-yoyo <quanthieucao@gmail.com>
2026-09-15 19:17:38 -0400

    Update menu.js
    Create reusable navigation component

--- menu.js
+++ menu.js
@@ -1,1 +1,63 @@
+$(document).ready(function () {
+
+  // Get the filename of the page currently open.
+  let currentPage = window.location.pathname.split("/").pop();
+
+  // GitHub Pages may open the homepage without showing index.html.
+  if (currentPage === "") {
+    currentPage = "index.html";
+  }
+
+  // Load the navigation information from menu.json.
+  $.getJSON("data/menu.json")
+    .done(function (data) {
+
+      // Begin creating the navigation.
+      let menuHTML = `
+        <nav class="main-menu">
+          <div class="website-name">
+            QTC-yoyo
+          </div>
+
+          <div class="menu-links">
+      `;
+
+      // Repeat these instructions for every item in menu.json.
+      data.menuItems.forEach(function (item) {
+
+        // The active class will highlight the current page.
+        let activeClass = "";
+
+        if (item.link === currentPage) {
+          activeClass = "active";
+        }
+
+        // Create a navigation link using the JSON information.
+        menuHTML += `
+          <a href="${item.link}" class="${activeClass}">
+            ${item.name}
+          </a>
+        `;
+      });
+
+      // Finish the navigation HTML.
+      menuHTML += `
+          </div>
+        </nav>
+      `;
+
+      // Display the finished navigation inside menu-container.
+      $("#menu-container").html(menuHTML);
+    })
+
+    // Display an error if menu.json cannot be loaded.
+    .fail(function () {
+      $("#menu-container").html(`
+        <p class="menu-error">
+          The navigation menu could not be loaded.
+        </p>
+      `);
+    });
+
+});
 
